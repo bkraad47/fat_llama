4886a15041776e503119062c0afeda365777e664
bkraad47 <bulkguy47@gmail.com>
2024-07-22 17:25:22 +1000

    v-0.1.6 update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fat_llama',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         'numpy',
