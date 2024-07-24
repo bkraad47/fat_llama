efd0b1dc47d1050caebfd4251bed85fbc238c151
bkraad47 <bulkguy47@gmail.com>
2024-07-24 16:07:56 +1000

    v-0.1.8.0

--- example.py
+++ example.py
@@ -4,16 +4,20 @@ from fat_llama.audio_fattener.feed import upscale_mp3_to_flac
 upscale_mp3_to_flac(
     input_file_path='input_test.mp3',
     output_file_path_processed='output_test.flac',
-    max_iterations=1000,
+    max_iterations=800,
     threshold_value=0.6,
-    gain_factor=32.8,
+    gain_factor=132.8,
     reduction_profile=[
-        (5, 140, -22.4),
-        (1000, 10000, 36.4),
+        (1, 140, -72.5),
+        (1000, 10000, -216),
     ],
-    lowcut=5.0,
-    highcut=150000.0,
+    lowcut=1.0,
+    highcut=80000.0,
     target_bitrate_kbps=1400,
     output_file_path_no_processing=None,
-    use_wiener_filter=False
+    toggle_wiener_filter=False,
+    toggle_normalize=True,       # Toggle normalization
+    toggle_equalize=False,        # Toggle equalization
+    toggle_scale_amplitude=False, # Toggle amplitude scaling
+    toggle_gain_reduction=False   # Toggle gain reduction
 )
