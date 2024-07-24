2479c109467cc9a1f83131445f085e953eb57862
bkraad47 <bulkguy47@gmail.com>
2024-07-24 23:43:49 +1000

    Update main method to only handle auto-scaling and fft. Also add different format support

--- example.py
+++ example.py
@@ -1,23 +1,12 @@
-from fat_llama.audio_fattener.feed import upscale_mp3_to_flac
+from fat_llama.audio_fattener.feed import upscale
 
 # Example call to the method
-upscale_mp3_to_flac(
+upscale(
     input_file_path='input_test.mp3',
-    output_file_path_processed='output_test.flac',
-    max_iterations=800,
+    output_file_path='output_test.flac',
+    source_format='mp3',
+    target_format='flac',
+    max_iterations=1000,
     threshold_value=0.6,
-    gain_factor=132.8,
-    reduction_profile=[
-        (1, 140, -72.5),
-        (1000, 10000, -216),
-    ],
-    lowcut=1.0,
-    highcut=80000.0,
-    target_bitrate_kbps=1400,
-    output_file_path_no_processing=None,
-    toggle_wiener_filter=False,
-    toggle_normalize=True,       # Toggle normalization
-    toggle_equalize=False,        # Toggle equalization
-    toggle_scale_amplitude=False, # Toggle amplitude scaling
-    toggle_gain_reduction=False   # Toggle gain reduction
+    target_bitrate_kbps=1411
 )
